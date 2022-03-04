3d129fe29c92f03d872d3d4e61b3fe787c8ff38e
Judy <judylee0428@gmail.com>
2022-03-04 17:24:04 +0800

    add api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-
 var app = new Vue({
-    el: "#search",
+    el: "#player",
     data:{
         query: "",
         musicList: []       
     },
     methods:{
         searchMusic: function(){
+            var that = this;
             axios.get("https://autumnfish.cn/search?keywords=" + this.query)
             .then(function(response){
+                that.musicList = response.data.result.songs;
                 console.log(response);
             }, function(err){})
         }
