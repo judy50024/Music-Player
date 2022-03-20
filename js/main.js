4e59a707f0ca8117e65c9407e4b0e612d3d6e7aa
Judy <judylee0428@gmail.com>
2022-03-20 22:23:29 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: "#player",
     data:{
         query: "",
-        musicList: []       
+        musicList: [],
+        musicUrl: "",    
     },
     methods:{
         searchMusic: function(){
@@ -10,12 +11,18 @@ var app = new Vue({
             axios.get("https://autumnfish.cn/search?keywords=" + this.query)
             .then(function(response){
                 that.musicList = response.data.result.songs;
-                console.log(response);
+                // console.log(response.data.result.songs);
             }, function(err){})
         },
-        // playMusic: function(){
-
-        // }
+        playMusic: function(musicId){
+            console.log(musicId);
+            var that = this;
+            axios.get("https://autumnfish.cn/song/url?id=" + musicId)
+            .then(function(response){
+                // console.log(response.data.data[0].url);
+                that.musicUrl = response.data.data[0].url;
+            }, function(err){})
+        }
 
     }
 })
